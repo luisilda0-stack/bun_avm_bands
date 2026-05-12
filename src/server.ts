2b0257983b4788c72eb9de20bb59ac64a51de15f
Luisa Tovar <luisilda0@gmail.com>
2026-05-12 16:22:39 +0200

    luitomen: Eventos del servidor de socket

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { Server } from "socket.io";
 import { Server as Engine } from "@socket.io/bun-engine";
 import { SERVER_CONFIG } from "./config/server-config";
-
+import { bandsService } from "./services/bands.service";
 
 export const creareServer = () => {
 
@@ -12,25 +12,61 @@ export const creareServer = () => {
   io.bind(engine);
 
   io.on("connection", (socket) => {
+
     console.log(`Cliente conectado (socket.id): ${socket.id}`);
 
     socket.emit("saludo", "Hola desde el servidor");
 
-    socket.on("chat", (msg)=> io.emit("chat", msg));
-})
+    socket.on("chat", (msg) => {
+      io.emit("chat", msg);
+    });
 
-io.on("disconnect", (socket) => {
-    console.log(`Cliente desconectado (socket.id): ${socket.id}`);
-    
-  })
-  
+    socket.emit("BANDS_LIST", bandsService.obtinereBands());
+
+    socket.on("ADD_BAND", (payload: { nomen: string }) => {
+
+      if (payload.nomen.trim() === "") return;
+
+      const band = bandsService.addereBand(payload.nomen);
+
+      io.emit("BANDS_LIST", bandsService.obtinereBands());
+
+    });
+
+    socket.on("VOTE_BAND", (payload: { id: string }) => {
+
+      const band = bandsService.addereVotumBand(payload.id);
+
+      if (band) {
+        io.emit("BANDS_LIST", bandsService.obtinereBands());
+      }
+
+    });
+
+    socket.on("DELETE_BAND", (payload: { id: string }) => {
+
+      const band = bandsService.delereBand(payload.id);
+
+      io.emit("BANDS_LIST", bandsService.obtinereBands());
+
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`Cliente desconectado (socket.id): ${socket.id}`);
+    });
+
+  });
+
   const { fetch: engineFetch, websocket } = engine.handler();
 
   const server = Bun.serve({
+
     port: SERVER_CONFIG.port,
     idleTimeout: SERVER_CONFIG.idleTimeout,
     websocket,
+
     fetch(req: Request, server: Parameters<typeof engineFetch>[1]) {
+
       const url = new URL(req.url);
 
       if (url.pathname.startsWith(SERVER_CONFIG.path)) {
@@ -45,8 +81,9 @@ io.on("disconnect", (socket) => {
       );
 
     },
+
   });
 
   return server;
-  
-}
+
+};
